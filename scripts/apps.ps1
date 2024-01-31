a92abb34cd8f9b9b2157d0d4605e6ec06df02a09
Kouei <hungjui11@gmail.com>
2024-01-31 09:43:33 +0800

    format

--- scripts/apps.ps1
+++ scripts/apps.ps1
@@ -1,19 +1,12 @@
 # Define the environment variables
 $apps = @{
-    "chrome" = "http://dl.google.com/chrome/install/375.126/chrome_installer.exe
-"
-    "7zip" = "https://www.7-zip.org/a/7z2301-x64.exe
-"
-    "skype" = "https://go.skype.com/windows.desktop.download
-"
-    "line" = "https://desktop.line-scdn.net/win/new/LineInst.exe
-"
-    "anydesk" = "https://anydesk.com/zht/downloads/thank-you?dv=win_exe
-"
-    "adobe" = "https://get.adobe.com/tw/reader/download?os=Windows+10&name=Reader+2023.006.20380+TradChines+Windows%2864Bit%29&lang=tw&nativeOs=Windows+10&accepted=mss&declined=&preInstalled=&site=landing
-"
-    "wireguard" = "https://download.wireguard.com/windows-client/wireguard-installer.exe
-"
+    "chrome" = "http://dl.google.com/chrome/install/375.126/chrome_installer.exe"
+    "7zip" = "https://www.7-zip.org/a/7z2301-x64.exe"
+    "skype" = "https://go.skype.com/windows.desktop.download"
+    "line" = "https://desktop.line-scdn.net/win/new/LineInst.exe"
+    "anydesk" = "https://anydesk.com/zht/downloads/thank-you?dv=win_exe"
+    "adobe" = "https://get.adobe.com/tw/reader/download?os=Windows+10&name=Reader+2023.006.20380+TradChines+Windows%2864Bit%29&lang=tw&nativeOs=Windows+10&accepted=mss&declined=&preInstalled=&site=landing"
+    "wireguard" = "https://download.wireguard.com/windows-client/wireguard-installer.exe"
 }
 
 # Loop through each URL and start the download process
