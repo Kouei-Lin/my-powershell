2545c1eefc6c8fd54df15b7a8487d9c8ef0e2059
Kouei <hungjui11@gmail.com>
2024-03-08 09:43:35 +0800

    adobe

--- scripts/apps.ps1
+++ scripts/apps.ps1
@@ -5,7 +5,7 @@ $apps = @{
     "skype" = "https://go.skype.com/windows.desktop.download"
     "line" = "https://desktop.line-scdn.net/win/new/LineInst.exe"
     "anydesk" = "https://anydesk.com/zht/downloads/thank-you?dv=win_exe"
-    "adobe" = "https://get.adobe.com/tw/reader/download?os=Windows+10&name=Reader+2023.006.20380+TradChines+Windows%2864Bit%29&lang=tw&nativeOs=Windows+10&accepted=mss&declined=&preInstalled=&site=landing"
+    "adobe" = "https://get.adobe.com/tw/reader/download?os=Windows+10&name=Reader+2023.008.20533+TradChines+Windows%2864Bit%29&lang=tw&nativeOs=Windows+10&accepted=&declined=mss&preInstalled=&site=landing"
     "wireguard" = "https://download.wireguard.com/windows-client/wireguard-installer.exe"
 }
 
