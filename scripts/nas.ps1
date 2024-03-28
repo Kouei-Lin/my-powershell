5854ce841b8006668be6e5295b5cde61af8b92f2
Kouei <hungjui11@gmail.com>
2024-03-28 13:41:03 +0800

    pass

--- scripts/nas.ps1
+++ scripts/nas.ps1
@@ -1,2 +1,2 @@
-#New-PSDrive –Name 'K' –PSProvider FileSystem –Root '\\touchsmart\share' –Persist
+New-PSDrive -Name "S" -PSProvider "FileSystem" -Root "\\192.168.xxx.xxx\xxx" -Credential (Get-Credential) -Persist
 #Remove-PSDrive -Name 'K'
