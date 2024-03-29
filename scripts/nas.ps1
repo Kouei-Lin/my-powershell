fb4198e5e892e022ef90c0d65b7d45d961dc3aba
Kouei <hungjui11@gmail.com>
2024-03-29 08:41:00 +0800

    driver

--- scripts/nas.ps1
+++ scripts/nas.ps1
@@ -1,2 +1,2 @@
-New-PSDrive -Name "A" -PSProvider "FileSystem" -Root "\\192.168.xxx.xxx\xxx" -Credential (Get-Credential) -Persist
-#Remove-PSDrive -Name 'A'
+New-PSDrive -Name "X" -PSProvider "FileSystem" -Root "\\192.168.xxx.xxx\xxx" -Credential (Get-Credential) -Persist
+#Remove-PSDrive -Name 'X'
