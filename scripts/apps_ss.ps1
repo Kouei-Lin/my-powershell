9fd8f483161d01644b7b2fe3a0e810762bf68a2d
Kouei <hungjui11@gmail.com>
2024-03-29 15:41:12 +0800

    mod

--- scripts/apps_ss.ps1
+++ scripts/apps_ss.ps1
@@ -1,24 +1,32 @@
-# Define the URL of the webpage you want to open
-$url = "https://www.google.com"
+# Define the URLs you want to open
+$urls = @(
+    "google.com"
+    "en.wikipedia.org",
+)
 
-# Launch Microsoft Edge in private mode and navigate to the specified URL
-Start-Process msedge.exe -ArgumentList "-inprivate", $url
+# Define base directory for saving screenshots
+$baseDirectory = "D:\PATH"
 
-# Wait for Edge to load the webpage (adjust the sleep time as needed)
-Start-Sleep -Seconds 5
+# Loop through each URL
+foreach ($url in $urls) {
+    # Launch Microsoft Edge in private mode and navigate to the specified URL
+    Start-Process msedge.exe -ArgumentList $url
 
-#Define date
-$date = Get-Date -Format "yyyyMMdd_HHmmss"
+    # Wait for Edge to load the webpage (adjust the sleep time as needed)
+    Start-Sleep -Seconds 30
 
-# Define the path where you want to save the screenshot
-$screenshotPath = "C:\Users\User\Desktop\$date.png"
+    # Define date
+    $date = Get-Date -Format "yyyyMMdd"
 
-# Take a screenshot of the entire screen and save it to the specified path
-Add-Type -AssemblyName System.Windows.Forms
-Add-Type -AssemblyName System.Drawing
-$screen = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds
-$screenshot = New-Object System.Drawing.Bitmap $screen.width, $screen.height
-$graphics = [System.Drawing.Graphics]::FromImage($screenshot)
-$graphics.CopyFromScreen([System.Drawing.Point]::Empty, [System.Drawing.Point]::Empty, $screen.size)
-$screenshot.Save($screenshotPath)
+    # Define the path where you want to save the screenshot
+    $screenshotPath = Join-Path -Path $baseDirectory -ChildPath "$url.png"
 
+    # Take a screenshot of the entire screen and save it to the specified path
+    Add-Type -AssemblyName System.Windows.Forms
+    Add-Type -AssemblyName System.Drawing
+    $screen = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds
+    $screenshot = New-Object System.Drawing.Bitmap $screen.width, $screen.height
+    $graphics = [System.Drawing.Graphics]::FromImage($screenshot)
+    $graphics.CopyFromScreen([System.Drawing.Point]::Empty, [System.Drawing.Point]::Empty, $screen.size)
+    $screenshot.Save($screenshotPath)
+}
